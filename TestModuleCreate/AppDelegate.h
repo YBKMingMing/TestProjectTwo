ebd27aed6776a916ca2f013988b9c800e833b0b8
YBKMingMing <18317713147@163.com>
2020-02-13 15:44:38 +0800

    Initial Commit

---
+++ TestModuleCreate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestModuleCreate
+//
+//  Created by cm on 2020/2/13.
+//  Copyright © 2020 cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestModuleCreate/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TestModuleCreate
+//
+//  Created by cm on 2020/2/13.
+//  Copyright © 2020 cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
